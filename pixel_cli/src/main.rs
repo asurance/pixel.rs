aa53299826f9d2cd2a15f0888683fa7c5e6170bc
asurance <784672616@qq.com>
2022-08-03 01:34:18 +0800

    初始化项目

---
+++ pixel_cli/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Hello, world!");
+    let config = pixel_core::Config { k: 32, size: 4 };
+    println!("{:?}", config);
+}
---
+++ pixel_core/src/lib.rs
@@ -0,0 +1,5 @@
+#[derive(Debug)]
+pub struct Config{
+    pub k:usize,
+    pub size:usize,
+}
